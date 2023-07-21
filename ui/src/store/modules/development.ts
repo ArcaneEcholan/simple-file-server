233cae7768235f8be93f31070be37f486245717b
wenchao <aronstrider@gmail.com>
2023-07-21 13:26:45 +0800

    [wc/user] support add user

---
+++ ui/src/store/modules/development.ts
@@ -0,0 +1,17 @@
+let debugView = true;
+// debugView = false;
+const state = {
+    debugView: debugView,
+    logEnabled: true
+};
+
+const mutations = {};
+
+const actions = {};
+
+export default {
+    namespaced: true,
+    state,
+    mutations,
+    actions,
+};
--- ui/src/views/user/index.vue
+++ ui/src/views/user/index.vue
@@ -1,31 +1,82 @@
 <template>
-<div class=''>
-    <el-card>
-        <template #header>
+    <div class=''>
+        <el-card>
+            <template #header>
 
-        </template>
-        <el-table :data="userList">
-            <el-table-column label="id" prop="id"></el-table-column>
-            <el-table-column label="username" prop="username"></el-table-column>
-            <el-table-column label="accDir" prop="accDir">
-                <template #default="scope">
-                    <el-input v-model="scope.row.accDir"></el-input>
-                    <el-button @click="onClickSaveAccDir(scope.row)">save</el-button>
-                </template>
-            </el-table-column>
-        </el-table>
-    </el-card>
-</div>
+            </template>
+            <el-button @click="onClickAddUser">add user</el-button>
+            <el-table :data="userList">
+                <el-table-column label="id" prop="id"></el-table-column>
+                <el-table-column label="username" prop="username"></el-table-column>
+                <el-table-column label="accDir" prop="accDir">
+                    <template #default="scope">
+                        <el-input v-model="scope.row.accDir"></el-input>
+                        <el-button @click="onClickSaveAccDir(scope.row)">save</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </el-card>
+
+        <el-dialog :visible.sync="addUserDialogVisible">
+            <debug-view>
+                <div>
+                    username:{{ newUsername }}
+                </div>
+                <div>
+                    password:{{ newPassword }}
+                </div>
+
+            </debug-view>
+            username
+            <el-input v-model="newUsername"></el-input>
+            password
+            <el-input v-model="newPassword"></el-input>
+            <el-button @click="confirmAddUser">confirm</el-button>
+        </el-dialog>
+    </div>
 </template>
 
 <script lang='ts'>
 import {Notification} from "element-ui";
-import { Component, Vue } from 'vue-property-decorator';
+import {Component, Vue} from 'vue-property-decorator';
 import Client from "@/request/client";
 import {get_token} from "@/ts/auth";
-@Component({})
+import DebugView from "@/components/DebugView.vue";
+
+@Component({
+    components: {DebugView}
+})
 export default class UserView extends Vue {
     userList: any[] = []
+
+    newUsername: string | null = null
+    newPassword: string | null = null
+    addUserDialogVisible = false
+    openAddUserDialog() {
+        this.addUserDialogVisible = true
+    }
+
+    closeAddUserDialog() {
+        this.addUserDialogVisible = false
+    }
+
+    onClickAddUser() {
+        this.openAddUserDialog()
+    }
+
+    confirmAddUser() {
+        let username = this.newUsername
+        let password = this.newPassword
+        Client.addUser(username!, password!, get_token()).then(resp => {
+            let user = resp.data
+            this.userList.push(user)
+            Notification.success({
+                title: 'success',
+                message: 'add user success'
+            })
+            this.closeAddUserDialog()
+        })
+    }
 
     created() {
         Client.getUserList(get_token()).then(resp => {
